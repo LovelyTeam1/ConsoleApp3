b7424247bf1dbf3c3872bdd007ec0209ddbd52f4
Mohammad Amin Moeini <MohammadAminMoeini@outlook.com>
2022-06-01 20:47:45 +0430

    add a username

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -7,6 +7,10 @@ namespace ConsoleApp3
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+
+            string userName = "Mohammad Amin";
+            Console.WriteLine(userName);
         }
     }
 }
